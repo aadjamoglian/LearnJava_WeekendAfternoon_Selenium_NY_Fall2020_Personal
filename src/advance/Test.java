83fcfd1686795a2a78073ff8a84941acde6087c4
Arthur <aadjamoglian@gmail.com>
2021-01-10 12:47:37 -0800

    First commit

---
+++ src/advance/Test.java
@@ -0,0 +1,18 @@
+package advance;
+
+import basic.LearnDataType;
+import basic.LearnStatic;
+
+public class Test {
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(LearnDataType.mobileBrand);
+		System.out.println(LearnStatic.stName);
+		
+		LearnStatic ls = new LearnStatic();
+		System.out.println(ls.stName);
+	}
+
+}
